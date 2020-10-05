1f93d8823c791a449beeefddc7cfdca1b696ebe3
ju-to <088t3748@gmail.com>
2020-10-06 04:14:05 +0900

    Rails課題2を回答

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+    redirect_to action: :index
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to action: :index
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to action: :index
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
